120a9e5c4e3d7b438299bbcbd4e928df1f4bf776
Kim youngOk <kyo2685@didimu.co.kr>
2023-07-06 14:43:44 +0900

    v2  commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header></Header>
       <Content></Content>
-      <Footer>하단입니다.</Footer>
+      <Footer>하단입니다.222</Footer>
        
     </div>
   );
