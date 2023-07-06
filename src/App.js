c28ea29e8a2586736a1c842ddbdba092d6b929c3
Kim youngOk <kyo2685@didimu.co.kr>
2023-07-06 14:15:05 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Header from './Header';
+import Content from './Content';
+import Footer from './Footer';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header></Header>
+      <Content></Content>
+      <Footer></Footer>
+       
+    </div>
+  );
+}
+
+export default App;
